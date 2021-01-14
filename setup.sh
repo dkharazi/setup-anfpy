352d4434b626af242ff23ae042343bf9e561209d
Darius Kharazi <dkharazif@gmail.com>
2021-01-14 18:07:38 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,21 @@
-# Download & Set Up Helm
+# https://zero-to-jupyterhub.readthedocs.io/en/latest/kubernetes/google/step-zero-gcp.html
 # https://zero-to-jupyterhub.readthedocs.io/en/latest/setup-helm.html
-curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
-kubectl --namespace kube-system create serviceaccount tiller
-kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller
-kubectl patch deployment tiller-deploy --namespace=kube-system --type=json --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/tiller", "--listen=localhost:44134"]}]'
+# https://zero-to-jupyterhub.readthedocs.io/en/latest/setup-jupyterhub.html
 
+# Download & Set Up Helm
+kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=dkharazif@gmail.com
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+# curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
+# kubectl --namespace kube-system create serviceaccount tiller
+# kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+# helm init --service-account tiller
+# kubectl patch deployment tiller-deploy --namespace=kube-system --type=json --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/tiller", "--listen=localhost:44134"]}]'
+
+# Add security token to config.yaml
+sed -i 's/<RANDOM_HEX>/'"$( openssl rand -hex 32 )"'/g' config.yaml
 
 # Set Up JupyterHub
-# https://zero-to-jupyterhub.readthedocs.io/en/latest/setup-jupyterhub.html
-sed -i 's/<RANDOM_HEX>/'"$( openssl rand -hex 32 )"'/g' config.yaml  # add security token to config.yaml
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo update
-helm upgrade --install jhub jupyterhub/jupyterhub --namespace jhub --version 0.8.2 --values config.yaml
+helm upgrade --cleanup-on-fail --install jhub jupyterhub/jupyterhub --namespace jhub --create-namespace --version=0.10.6 --values config.yaml
+# helm upgrade --install jhub jupyterhub/jupyterhub --namespace jhub --version 0.8.2 --values config.yaml
